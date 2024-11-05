720827238d8f33aea574c8cff21ed15e7eaee7ff
Jawher Allani <allajaw@amazon.de>
2024-11-05 10:36:40 +0100

    merchant fulfilment cdk migration

---
+++ use-cases/merchant-fulfillment/app/sp-api-app-cdk/bin/sp-api-app-cdk.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import {SpApiAppCdkStack} from '../lib/sp-api-app-cdk-stack';
+
+const app = new cdk.App();
+new SpApiAppCdkStack(app, 'sp-api-app-cdk', {
+    randomSuffix: app.node.tryGetContext("RANDOM_SUFFIX"),
+    spApiCdkQualifier: app.node.tryGetContext("CDK_QUALIFIER")
+});
