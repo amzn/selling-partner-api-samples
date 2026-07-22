39fcb2c4c5bbf09afc7c5a5addc9e66310eb0459
mafge <mfge@amazon.de>
2026-07-22 11:36:23 +0200

    Remove deprecated solutions

--- use-cases/pricing-b2b/app/scripts/java/java-app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Execute the generic deployment script. Use "java" for programming language argument
-bash ../shared/deploy-app.sh -l "java"
--- use-cases/solicitations/code/python/src/api_models/notifications_api/swagger_client/api/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from api_models.notifications_api.swagger_client.api.notifications_api import NotificationsApi
